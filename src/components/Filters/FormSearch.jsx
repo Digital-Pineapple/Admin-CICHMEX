3f82ad1aec0c324231d308daea478550f95424ff
RodrigoRPF <rodrigorpf.dp.com>
2024-10-29 10:20:50 -0600

    filer sugestions

--- src/components/Filters/FormSearch.jsx
+++ src/components/Filters/FormSearch.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { TextField, Box, Typography, List, ListItem, InputAdornment } from "@mui/material";
+import { TextField, Box, List, ListItem, InputAdornment } from "@mui/material";
 import { useSelector } from "react-redux";
 import { Search } from "@mui/icons-material";
 import Swal from "sweetalert2";
@@ -13,35 +13,41 @@ function FormSearch({ setSelected, allValues, titleAlert }) {
   function handleChange(e) {
     let value = e.target.value;
     setValue(value);
-    if (value.trim()) {
-      setShowSuggestions(true);
-    } else {
-      setShowSuggestions(false);
-    }
+    setShowSuggestions(value.trim() !== "");
   }
 
   function onClickSuggestion(suggestion) {
     if (allValues.includes(suggestion)) {
       Swal.fire({
-        title:titleAlert,
-        timer:2000,
-        icon:'error',
-        
-      })
-      return
+        title: titleAlert,
+        timer: 2000,
+        icon: "error",
+      });
+      return;
     }
     setSelected(suggestion);
-    setValue('');
+    setValue("");
     setShowSuggestions(false);
   }
 
   const filterSuggestions = () => {
-    return suggestions.filter((suggestion) => {
-      return (
-        suggestion?.name?.toLowerCase().includes(value?.toLowerCase().trim()) ||
-        suggestion?.tag?.toLowerCase().includes(value?.toLowerCase().trim())
-      );
-    });
+    const lowerCaseValue = value.toLowerCase().trim();
+    return suggestions
+      .filter((suggestion) => {
+        const nameMatch = suggestion?.name?.toLowerCase().includes(lowerCaseValue);
+        const tagMatch = suggestion?.tag?.toLowerCase().includes(lowerCaseValue);
+        return nameMatch || tagMatch;
+      })
+      .sort((a, b) => {
+        const aNameStartsWith = a.name.toLowerCase().startsWith(lowerCaseValue);
+        const bNameStartsWith = b.name.toLowerCase().startsWith(lowerCaseValue);
+        const aTagStartsWith = a.tag.toLowerCase().startsWith(lowerCaseValue);
+        const bTagStartsWith = b.tag.toLowerCase().startsWith(lowerCaseValue);
+
+        if (aNameStartsWith || aTagStartsWith) return -1;
+        if (bNameStartsWith || bTagStartsWith) return 1;
+        return 0;
+      });
   };
 
   const renderSuggestions = () => {
@@ -49,34 +55,29 @@ function FormSearch({ setSelected, allValues, titleAlert }) {
     if (filteredSuggestions.length === 0) {
       return <ListItem>No hay sugerencias</ListItem>;
     }
-    return filteredSuggestions.slice(0, 6)?.map((suggestion, index) => {
-      return (
-        <ListItem
-          onClick={() => onClickSuggestion(suggestion)}
-          sx={{
-            fontSize: "16px",
-            padding: "10px 5px 5px 10px",
-            cursor: "pointer",
-            backgroundColor: index === activeSuggestionIndex ? "#b0bec5" : "",
-          }}
-          key={index}
-        >
-          {suggestion.tag + "-" + suggestion.name}
-        </ListItem>
-      );
-    });
+    return filteredSuggestions.map((suggestion, index) => (
+      <ListItem
+        onClick={() => onClickSuggestion(suggestion)}
+        sx={{
+          fontSize: "16px",
+          padding: "10px 5px 5px 10px",
+          cursor: "pointer",
+          backgroundColor: index === activeSuggestionIndex ? "#b0bec5" : "",
+          fontWeight: index === 0 ? "bold" : "normal", 
+        }}
+        key={index}
+      >
+        {suggestion.tag + " - " + suggestion.name}
+      </ListItem>
+    ));
   };
 
   const handleKeyDown = (e) => {
     const filteredSuggestions = filterSuggestions();
-    if (e.key === "ArrowDown") {
-      if (activeSuggestionIndex < filteredSuggestions.length - 1) {
-        setActiveSuggestionIndex(activeSuggestionIndex + 1);
-      }
-    } else if (e.key === "ArrowUp") {
-      if (activeSuggestionIndex > 0) {
-        setActiveSuggestionIndex(activeSuggestionIndex - 1);
-      }
+    if (e.key === "ArrowDown" && activeSuggestionIndex < filteredSuggestions.length - 1) {
+      setActiveSuggestionIndex(activeSuggestionIndex + 1);
+    } else if (e.key === "ArrowUp" && activeSuggestionIndex > 0) {
+      setActiveSuggestionIndex(activeSuggestionIndex - 1);
     } else if (e.key === "Enter") {
       e.preventDefault();
       if (filteredSuggestions.length > 0) {
@@ -86,55 +87,54 @@ function FormSearch({ setSelected, allValues, titleAlert }) {
   };
 
   return (
-    <>
-      <Box
-        sx={{
-          display: "flex",
-          position: "relative",
-          flexDirection: "column",
-          justifyContent: "space-around",
-        }}
-      >
-        <div>
-          <TextField
-            id="name"
-            name="name"
-            variant="outlined"
-            size="medium"
-            value={value}
-            onChange={handleChange}
-            onKeyDown={handleKeyDown}
-            autoComplete="off"
-            fullWidth
-            placeholder="Nombre de producto"
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="start">
-                  <Search />
-                </InputAdornment>
-              )
+    <Box
+      sx={{
+        display: "flex",
+        position: "relative",
+        flexDirection: "column",
+        justifyContent: "space-around",
+      }}
+    >
+      <div>
+        <TextField
+          id="name"
+          name="name"
+          variant="outlined"
+          size="medium"
+          value={value}
+          onChange={handleChange}
+          onKeyDown={handleKeyDown}
+          autoComplete="off"
+          fullWidth
+          placeholder="Nombre de producto"
+          InputProps={{
+            endAdornment: (
+              <InputAdornment position="start">
+                <Search />
+              </InputAdornment>
+            ),
+          }}
+        />
+        {showSuggestions && (
+          <List
+            sx={{
+              position: "absolute",
+              zIndex: 2,
+              color: "white",
+              borderRadius: "4px",
+              padding: "0px",
+              margin: "0px",
+              backgroundColor: "#0d2b6b",
+              width: "100%",
+              maxHeight: "200px",
+              overflowY: "auto",
             }}
-          />
-          {showSuggestions && (
-            <List
-              sx={{
-                position: "absolute",
-                zIndex: 2,
-                listStyle: "none",
-                color: "white",
-                borderRadius: "4px",
-                padding: "0px",
-                margin: "0px",
-                backgroundColor: "#0d2b6b",
-                width: "100%",
-              }}
-            >
-              {renderSuggestions()}
-            </List>
-          )}
-        </div>
-      </Box>
-    </>
+          >
+            {renderSuggestions()}
+          </List>
+        )}
+      </div>
+    </Box>
   );
 }
 
--- src/pages/Products/StepNewProduct/Variants.jsx
+++ src/pages/Products/StepNewProduct/Variants.jsx
@@ -4,7 +4,13 @@ import CardContent from "@mui/material/CardContent";
 import CardActions from "@mui/material/CardActions";
 import Button from "@mui/material/Button";
 import CardHeader from "@mui/material/CardHeader";
-import { Add, Delete, ExpandLess, ExpandMore } from "@mui/icons-material";
+import {
+  Add,
+  Delete,
+  ExpandLess,
+  ExpandMore,
+  UploadFile,
+} from "@mui/icons-material";
 import {
   Collapse,
   List,
@@ -17,14 +23,20 @@ import {
   TextField,
   FormControlLabel,
   Checkbox,
+  FormControl,
+  FormLabel,
+  FormHelperText,
+  Box,
 } from "@mui/material";
 import Swal from "sweetalert2";
 import { Controller, useForm } from "react-hook-form";
 import ColorSelector from "../../../components/inputs/ColorSelector";
+import { blue } from "@mui/material/colors";
 
 const Variants = ({ handleNext, handleBack, index, isLastStep }) => {
   const [valueVariants, setValueVariants] = useState([]); // Array to hold variants
   const [collapseOpen, setCollapseOpen] = useState([]); // Array to track the open/close state of each variant
+  const [productImages, setproductImages] = useState([]);
 
   const {
     control,
@@ -50,16 +62,16 @@ const Variants = ({ handleNext, handleBack, index, isLastStep }) => {
   const handleAddVariant = () => {
     const newId = valueVariants.length;
     setValueVariants([...valueVariants, { id: newId }]); // Add new variant with id
-    setCollapseOpen([...collapseOpen, { id: newId, value: false }]); // Set collapse as closed for new variant
+    setCollapseOpen([...collapseOpen, { id: newId, value: true }]); // Set collapse as closed for new variant
 
     // Initialize form values for the new variant
     setValue(`variants[${newId}]`, {
       sku: "",
       weight: "",
       design: { textInput: "", checkbox: false },
-      tag:{textInput: "", checkbox: false},
+      tag: { textInput: "", checkbox: false },
       color: "",
-
+      images: [],
     });
   };
 
@@ -100,20 +112,30 @@ const Variants = ({ handleNext, handleBack, index, isLastStep }) => {
     }
   };
   const fieldValues = watch("variants");
+  
+  
+  
 
-  const onSubmit = (values) =>{
-    console.log(values,'subir info');
-    
-  }
+  const onSubmit = (values) => {
+    console.log(values, "subir info");
+  };
 
   return (
-    <Card variant="elevation" component={'form'} onSubmit={handleSubmit(onSubmit)}>
+    <Card
+      variant="elevation"
+      component={"form"}
+      onSubmit={handleSubmit(onSubmit)}
+    >
       <CardHeader
         title="Variantes y fotos"
         subheader="Suma color, fotos, tallas de la guía seleccionada, cantidad y otros datos específicos para cada variante del producto."
       />
       <CardContent>
-        <Button variant="contained" color="secondary" onClick={handleAddVariant}>
+        <Button
+          variant="contained"
+          color="secondary"
+          onClick={handleAddVariant}
+        >
           <Add /> Agregar variante
         </Button>
         <List
@@ -128,7 +150,7 @@ const Variants = ({ handleNext, handleBack, index, isLastStep }) => {
         >
           {valueVariants.map((item, index) => {
             const isOpen = collapseOpen.find((el) => el.id === item.id)?.value;
-            
+
             return (
               <div key={item.id}>
                 <ListItem
@@ -155,107 +177,216 @@ const Variants = ({ handleNext, handleBack, index, isLastStep }) => {
                 <Collapse in={isOpen} timeout="auto" unmountOnExit>
                   <Grid container spacing={2}>
                     <Grid item xs={6}>
-                      
-                    <Controller
-                      control={control}
-                      name={`variants[${index}].design.textInput`}
-                      rules={{ required: { value:!watch(`variants[${item.id}].design.checkbox`), message: "Campo requerido" } }}
-                      render={({ field }) => (
-                        <TextField
-                          {...field}
-                          fullWidth
-                          disabled={watch(`variants[${index}].design.checkbox`)}
-                          label="Diseño"
-                          error={!!errors.variants?.[index]?.design?.textInput}
-                          helperText={errors.variants?.[index]?.design?.textInput?.message}
-                        />
-                      )}
-                    />
+                      <Controller
+                        control={control}
+                        name={`variants[${index}].design.textInput`}
+                        rules={{
+                          required: {
+                            value: !watch(
+                              `variants[${item.id}].design.checkbox`
+                            ),
+                            message: "Campo requerido",
+                          },
+                        }}
+                        render={({ field }) => (
+                          <TextField
+                            {...field}
+                            fullWidth
+                            disabled={watch(
+                              `variants[${index}].design.checkbox`
+                            )}
+                            label="Diseño"
+                            error={
+                              !!errors.variants?.[index]?.design?.textInput
+                            }
+                            helperText={
+                              errors.variants?.[index]?.design?.textInput
+                                ?.message
+                            }
+                          />
+                        )}
+                      />
 
-                    <Controller
-                      name={`variants[${index}].design.checkbox`}
-                      control={control}
-                      render={({ field: { onChange, name } }) => (
-                        <FormControlLabel
-                          componentsProps={{
-                            typography: { variant: "body2" },
-                          }}
-                          control={
-                            <Checkbox
-                              checked={watch(`variants[${index}].design.checkbox`)}
-                              size="small"
-                              onChange={(e) => {
-                                onChange(e.target.checked);
-                                handleCheckboxChange(`variants[${index}].design.textInput`, e.target.checked, index);
-                              }}
-                            />
-                          }
-                          label="No aplica *"
-                        />
-                      )}
-                    />
+                      <Controller
+                        name={`variants[${index}].design.checkbox`}
+                        control={control}
+                        render={({ field: { onChange, name } }) => (
+                          <FormControlLabel
+                            componentsProps={{
+                              typography: { variant: "body2" },
+                            }}
+                            control={
+                              <Checkbox
+                                checked={watch(
+                                  `variants[${index}].design.checkbox`
+                                )}
+                                size="small"
+                                onChange={(e) => {
+                                  onChange(e.target.checked);
+                                  handleCheckboxChange(
+                                    `variants[${index}].design.textInput`,
+                                    e.target.checked,
+                                    index
+                                  );
+                                }}
+                              />
+                            }
+                            label="No aplica *"
+                          />
+                        )}
+                      />
                     </Grid>
                     <Grid item xs={6}>
                       <ColorSelector />
                     </Grid>
                     <Grid item xs={6}>
-                    <Controller
-                      control={control}
-                      name={`variants[${index}].tag.textInput`}
-                      rules={{ required: { value:!watch(`variants[${item.id}].tag.checkbox`), message: "Campo requerido" } }}
-                      render={({ field }) => (
-                        <TextField
-                          {...field}
-                          fullWidth
-                          disabled={watch(`variants[${index}].tag.checkbox`)}
-                          label="Codigo universal de producto"
-                          error={!!errors.variants?.[index]?.tag?.textInput}
-                          helperText={errors.variants?.[index]?.tag?.textInput?.message}
-                        />
-                      )}
-                    />
+                      <Controller
+                        control={control}
+                        name={`variants[${index}].tag.textInput`}
+                        rules={{
+                          required: {
+                            value: !watch(`variants[${item.id}].tag.checkbox`),
+                            message: "Campo requerido",
+                          },
+                        }}
+                        render={({ field }) => (
+                          <TextField
+                            {...field}
+                            fullWidth
+                            disabled={watch(`variants[${index}].tag.checkbox`)}
+                            label="Codigo universal de producto"
+                            error={!!errors.variants?.[index]?.tag?.textInput}
+                            helperText={
+                              errors.variants?.[index]?.tag?.textInput?.message
+                            }
+                          />
+                        )}
+                      />
 
-                    <Controller
-                      name={`variants[${index}].tag.checkbox`}
-                      control={control}
-                      render={({ field: { onChange, name } }) => (
-                        <FormControlLabel
-                          componentsProps={{
-                            typography: { variant: "body2" },
-                          }}
-                          control={
-                            <Checkbox
-                              checked={watch(`variants[${index}].tag.checkbox`)}
-                              size="small"
-                              onChange={(e) => {
-                                onChange(e.target.checked);
-                                handleCheckboxChange(`variants[${index}].tag.textInput`, e.target.checked, index);
-                              }}
-                            />
-                          }
-                          label="Mi producto no lo tiene*"
-                        />
-                      )}
-                    />
+                      <Controller
+                        name={`variants[${index}].tag.checkbox`}
+                        control={control}
+                        render={({ field: { onChange, name } }) => (
+                          <FormControlLabel
+                            componentsProps={{
+                              typography: { variant: "body2" },
+                            }}
+                            control={
+                              <Checkbox
+                                checked={watch(
+                                  `variants[${index}].tag.checkbox`
+                                )}
+                                size="small"
+                                onChange={(e) => {
+                                  onChange(e.target.checked);
+                                  handleCheckboxChange(
+                                    `variants[${index}].tag.textInput`,
+                                    e.target.checked,
+                                    index
+                                  );
+                                }}
+                              />
+                            }
+                            label="Mi producto no lo tiene*"
+                          />
+                        )}
+                      />
                     </Grid>
                     <Grid item xs={6}>
-                    <Controller
-                      control={control}
-                      rules={{ required: { value:!watch(`variants[${index}].sku`), message: "Campo requerido" } }}
-                      name={`variants[${index}].sku`}
-                      render={({ field }) => (
-                        <TextField
-                          {...field}
-                          fullWidth
-                          label="SKU"
-                          autoComplete="off"
-                          error={!!errors.variants?.[index]?.sku}
-                          helperText={errors.variants?.[index]?.sku.message}
-                        />
-                      )}
-                    />
+                      <Controller
+                        control={control}
+                        rules={{
+                          required: {
+                            value: !watch(`variants[${index}].sku`),
+                            message: "Campo requerido",
+                          },
+                        }}
+                        name={`variants[${index}].sku`}
+                        render={({ field }) => (
+                          <TextField
+                            {...field}
+                            fullWidth
+                            label="SKU"
+                            autoComplete="off"
+                            error={!!errors.variants?.[index]?.sku}
+                            helperText={errors.variants?.[index]?.sku.message}
+                          />
+                        )}
+                      />
                     </Grid>
-                    
+                    <Grid item xs={12}>
+                      <FormControl>
+                        <FormLabel>Fotos (requerido)</FormLabel>
+                        <Controller
+                          control={control}
+                          rules={{
+                            required: {
+                              value: !watch(`variants[${index}].images`),
+                              message: "Campo requerido",
+                            },
+                          }}
+                          name={`variants[${index}].images`}
+                          render={({
+                            field: { name, ref, onChange, onBlur },
+                          }) => (
+                            <Box
+                              width="fit-content"
+                              height="fit-content"
+                              display="flex"
+                              alignItems="center"
+                              justifyContent="center"
+                              component="label"
+                              htmlFor="imagesInput"
+                              sx={{ cursor: "pointer" }}
+                            >
+                              <Box
+                                sx={{
+                                  position: "relative",
+                                  backgroundColor: "#e1f5fe",
+                                  padding: "30px 70px",
+                                  borderRadius: "20px",
+                                  border: "2px dashed #bbdefb",
+                                  boxShadow: "0px 0px 200px -50px #90caf9",
+                                  textAlign: "center",
+                                  transition:
+                                    "background-color 0.3s ease-in-out",
+                                  "&:hover": {
+                                    backgroundColor: "#bbdefb",
+                                    border: "2px dashed #42a5f5",
+                                  },
+                                  
+                                }}
+                              >
+                                <Typography variant="body2" color="inherit">
+                                  <UploadFile style={{ color: "#42a5f5" }} />{" "}
+                                  <strong style={{ color: "#42a5f5" }}>
+                                    Seleccionar o arrastrar los archivos aquí
+                                  </strong>
+                                  <br />
+                                  Sube tu imagen en JPG, JPEG o PNG , con
+                                  una resolución mínima de 50 píxeles en ambos
+                                  lados y hasta 10 MB de peso.
+                                </Typography>
+                                <input
+                                  id="imagesInput"
+                                  type="file"
+                                  multiple
+                                  ref={ref}
+                                  onBlur={onBlur}
+                                  name={name}
+                                  onChange={(e) => onChange({...watch(`variants[${index}].images)`, e.target.value)})}
+                                  style={{ display: "none" }}
+                                  accept="image/png, image/jpeg,"
+                                />
+                              </Box>
+                            </Box>
+                          )}
+                        />
+                        <FormHelperText error={!!errors.variants?.[index]?.images}>
+                          {!!errors.variants?.[index]?.images.message}
+                        </FormHelperText>
+                      </FormControl>
+                    </Grid>
 
                     {/* Example of new fields for each variant */}
                     <Controller
@@ -279,7 +410,11 @@ const Variants = ({ handleNext, handleBack, index, isLastStep }) => {
         </List>
       </CardContent>
       <CardActions>
-        <Button disabled={index === 0} onClick={handleBack} sx={{ mt: 1, mr: 1 }}>
+        <Button
+          disabled={index === 0}
+          onClick={handleBack}
+          sx={{ mt: 1, mr: 1 }}
+        >
           Cancelar
         </Button>
         <Button variant="contained" type="submit" sx={{ mt: 1, mr: 1 }}>
